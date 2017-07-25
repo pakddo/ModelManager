d4e91264959f5cc7c01fd071eeb7405c57d468e3
Dooho Park <pakddo@gmail.com>
2017-07-25 16:59:15 +0900

    unit test for url mapping and temp view

--- manager/tests.py
+++ manager/tests.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from manager.views import home_page
 
-# Create your tests here.
+
+class SmokeTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
