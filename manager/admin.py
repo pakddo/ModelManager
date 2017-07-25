196ab46c70106896661bea87f90191edc7ba7b76
Dooho Park <pakddo@gmail.com>
2017-07-25 12:10:18 +0900

    Admin Modification

--- manager/admin.py
+++ manager/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Model
+from .models import Model, Project
 
 admin.site.register(Model)
+admin.site.register(Project)
