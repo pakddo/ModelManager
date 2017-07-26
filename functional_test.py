c1fb07d28af8f482327e856540ee48f867e0cc49
Dooho Park <pakddo@gmail.com>
2017-07-26 11:43:23 +0900

    functional_test.py modified

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
 
@@ -21,42 +26,19 @@ class NewVisitorTest(unittest.TestCase):
 
         #add something
         inputbox = self.browser.find_element_by_id('id_new_item')
-        # self.assertEqual(
-        #     inputbox.get_attribute('placeholder'),
-        #     'Enter a project item'
-        # )
-
         inputbox.send_keys('Recommend Model Making')
-
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Recommend Model Making' for row in rows),
-        #     "No new item in table -- TEST TEXT:\n%s" % (
-        #         table.text,
-        #     )
-        # )
-        self.assertIn('1: Recommend Model Making', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1. Recommend Model Making')
 
         #add something
         inputbox = self.browser.find_element_by_id('id_new_item')
-        # self.assertEqual(
-        #     inputbox.get_attribute('placeholder'),
-        #     'Enter a project item'
-        # )
-
         inputbox.send_keys('Making service using Recommend Model')
-
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Recommend Model Making', [row.text for row in rows])
-        self.assertIn('2: Making service using Recommend Model', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('2. Making service using Recommend Model')
+        self.check_for_row_in_list_table('1. Recommend Model Making')
+        
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
